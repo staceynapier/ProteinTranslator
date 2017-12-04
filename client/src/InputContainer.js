e5a6c730d1787ece06621ea18b626f2f37e5fbad
staceynapier <staceynapier86@gmail.com>
2017-12-04 21:53:42 +0000

    input form added to InputContainer, additional react files removed

--- client/src/InputContainer.js
+++ client/src/InputContainer.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class InputContainer extends Component {
+
+  handleSubmit() {
+    
+  }
+
   render() {
     return (
-      <p>InputContainer</p>
+      <article>
+        <h1>Protein Translator</h1>
+        <form>
+          <label>
+            Enter RNA sequence:
+            <input type="text" name="sequence" />
+          </label>
+          <input type="submit" value="Translate" onSubmit={this.handleSubmit}/>
+        </form>
+      </article>
     );
   }
 }
